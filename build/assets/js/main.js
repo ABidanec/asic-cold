319d64ea1ba745064a98a556df5e9a8bc9e1f22b
ABidanec <a.bidanec@ukr.net>
2018-02-12 21:40:12 +0200

    Реализация кнопки вверх

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -12,6 +12,8 @@ $(document).ready(function(){
     Circle('.circle');
 
     _moduleSlickMenu.action();
+    //_moduleSlider.action();
+    _scrollToTop.action();
 
 });
 
@@ -27,6 +29,58 @@ _moduleSlickMenu = (function(){
                prependTo: _prependTo
             });
             console.log('slick nav');
+        }
+    }
+})();
+
+_moduleSlider = (function(){
+    var _slider = $('.slider__list');
+
+    return {
+        action: function(){
+            _slider.slick({
+                dots: true,
+                autoplay: true,
+                autoplaySpeed: 1000,
+                fade: true,
+                pauseOnHover: true,
+                adaptiveHeight: true,
+                arrows: false,
+                draggable: true,
+                responsive: [
+                    {
+                        breakpoint: 600,
+                        settings: {
+                            dots: false
+                        }
+                    }
+                ]
+            });
+        }
+    }
+})();
+
+_scrollToTop = (function(){
+    var scroll = $(".scroll-top"),
+        top = 300,
+        delay = 800;
+    return {
+        action: function(){
+            scroll.fadeOut();
+            $(window).on("scroll",function() {
+                if($(window).scrollTop() >= top) {
+                    scroll.fadeIn();
+                } else {
+                    scroll.fadeOut();
+                }
+            });
+            scroll.on("click",function(e) {
+                e.preventDefault();
+                $('body,html').animate(
+                    {scrollTop:0},
+                    delay
+                );
+            });
         }
     }
 })();
